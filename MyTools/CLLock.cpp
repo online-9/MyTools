f85e09c17a2572886d8f0ac8071aab5ceab5c6f4
Lin <wy.exe@hotmail.com>
2017-04-22 02:44:58 +0800

    refactor CriticalSection to std::mutex in CLLock

--- MyTools/CLLock.cpp
+++ MyTools/CLLock.cpp
@@ -5,40 +5,29 @@
 #include "Character.h"
 #define _SELF L"CLLock.cpp"
 
-CLLock::CLLock(_In_ std::wstring wsLockName_) : wsLockName(wsLockName_)
+CLLock::CLLock(_In_ std::wstring wsLockName_) : _wsLockName(wsLockName_)
 {
-	//hMutex = ::CreateMutexW(NULL, FALSE, wsLockName_.c_str());
-	::InitializeCriticalSection(&LockSection);
+	
 }
 
 CLLock::~CLLock()
 {
-	//::ReleaseMutex(hMutex);
-	//::CloseHandle(hMutex);
-	//hMutex = NULL;
-	::DeleteCriticalSection(&LockSection);
+	
+	
 }
 
 void CLLock::Lock() CONST
 {
-	//if (WaitForSingleObject(hMutex, 15 * 1000) == WAIT_TIMEOUT)
-	//{
-	//	LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"Lock超时, LockName=%s", wsLockName.c_str());
-	//}
-	::EnterCriticalSection(&LockSection);
+	_Mutex.lock();
 }
 
 void CLLock::UnLock() CONST
 {
-	//::ReleaseMutex(hMutex);
-	::LeaveCriticalSection(&LockSection);
+	_Mutex.unlock();
 }
 
 BOOL CLLock::Access(std::function<void(void)> MethodPtr) CONST
 {
-	BOOL bRetCode = FALSE;
-	Lock();
-
 	auto fnAccess = [&MethodPtr,this]
 	{
 		__try
@@ -47,12 +36,12 @@ BOOL CLLock::Access(std::function<void(void)> MethodPtr) CONST
 		}
 		__except (1)
 		{
-			LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"Access发生了异常, LockName=%s", wsLockName.c_str());
+			LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"Access发生了异常, LockName=%s", _wsLockName.c_str());
 		}
 	};
-
+	Lock();
 	fnAccess();
-	bRetCode = TRUE;
 	UnLock();
-	return bRetCode;
+
+	return TRUE;
 }
